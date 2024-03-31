c70029090a607e794bd8bac26d34618c89eab881
GabyAM <gdgabyam@gmail.com>
2024-03-31 16:48:03 -0300

    Implements auth on Layout component
    Since the layout component is the parent component for all the rest of the pages, i only have to implement the auth verification here.

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,18 +1,29 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigate } from 'react-router-dom';
 import { NavBar } from './NavBar';
 import '../styles/layout.css';
 import { Header } from './Header';
+import { useAuth } from '../hooks/useAuth';
+import { useEffect } from 'react';
 
 export function Layout() {
-    return (
-        <div className="main-layout">
-            <NavBar></NavBar>
-            <div className="content-container flex-col">
-                <Header></Header>
-                <div className="container flex-col">
-                    <Outlet></Outlet>
+    const navigate = useNavigate();
+    const { token, loading } = useAuth();
+    useEffect(() => {
+        if (!token && !loading) {
+            return navigate('/login');
+        }
+    }, [token, loading]);
+    if (token && !loading) {
+        return (
+            <div className="main-layout">
+                <NavBar></NavBar>
+                <div className="content-container flex-col">
+                    <Header></Header>
+                    <div className="container flex-col">
+                        <Outlet></Outlet>
+                    </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
 }

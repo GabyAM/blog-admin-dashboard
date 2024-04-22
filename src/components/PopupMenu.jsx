4a757b74a6876e8b4864db436e6f61e47a6812aa
GabyAM <gdgabyam@gmail.com>
2024-04-22 18:35:21 -0300

    Creates PopupMenu component
    this popup menu will receive its children and a function to dismount itself when clicked outside of it (the parent component has to dismount it since it's the one rendering it).
    The children, wich are supposed to be grid elements with an icon and a label, are displayed in a list.
    
    This is being added as a replacement of the action buttons on cards

---
+++ src/components/PopupMenu.jsx
@@ -0,0 +1,34 @@
+import { Fragment, useEffect, useRef, useState } from 'react';
+
+export function PopupMenu({ children, onClickOutside }) {
+    const ref = useRef(null);
+    const [isMounted, setIsMounted] = useState(false);
+    useEffect(() => {
+        setIsMounted(true);
+        function handleClickOutside(e) {
+            if (ref.current && !ref.current.contains(e.target)) {
+                onClickOutside();
+            }
+        }
+
+        if (isMounted) {
+            document.addEventListener('click', handleClickOutside);
+        }
+        return () => {
+            document.removeEventListener('click', handleClickOutside);
+            setIsMounted(false);
+        };
+    }, [isMounted]);
+    return (
+        <div ref={ref} className="popup-menu">
+            {children.map((element, index) => (
+                <Fragment key={index}>
+                    {element}
+                    {index < children.length - 1 && (
+                        <div className="popup-menu-separator"></div>
+                    )}
+                </Fragment>
+            ))}
+        </div>
+    );
+}

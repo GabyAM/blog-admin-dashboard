10804eb3a060e55d02501a5fb664ffa8cf19d558
GabyAM <gdgabyam@gmail.com>
2024-04-23 11:15:58 -0300

    Implements popup menu on post cards
    This replaces the action buttons section of the card.

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,41 +1,74 @@
 import { Link } from 'react-router-dom';
 import he from 'he';
 import '../styles/postcard.css';
+import { useState } from 'react';
+import {
+    DeleteIcon,
+    EditIcon,
+    PublishIcon,
+    UnpublishIcon,
+    VerticalDotsIcon
+} from './Icons';
+import { PopupMenu } from './PopupMenu';
 
 export function PostCard({ post, onToggleState }) {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
     return (
-        <div className="post-card">
-            <div className="main-section">
-                <div className="post-thumbnail">
-                    <img src={`http://localhost:3000${post.image}`}></img>
-                </div>
-                <div className="text-section flex-col">
-                    <h2 className="title-primary">{he.decode(post.title)}</h2>
-                    <p>{he.decode(post.summary)}</p>
-                </div>
+        <div className={`post-card ${post.isPending ? 'pending' : ''}`}>
+            <div className="post-thumbnail">
+                <img src={`http://localhost:3000${post.image}`}></img>
             </div>
-            <div className="actions flex-row">
-                <button className="action-button red-outline">Delete</button>
-                <Link
-                    className="action-button green-outline"
-                    to={`/post/${post._id}`}
+            <div className="text-section flex-col">
+                <h2 className="title-primary">{he.decode(post.title)}</h2>
+                <p>{he.decode(post.summary)}</p>
+            </div>
+            <div className="options-section">
+                <button
+                    disabled={post.isPending}
+                    onClick={() => setIsMenuOpen(true)}
+                    className="icon-container more-button"
                 >
-                    Edit
-                </Link>
-                {post.is_published ? (
-                    <button
-                        className="action-button secondary-button"
-                        onClick={(e) => onToggleState(e.target, post._id)}
-                    >
-                        Unpublish
-                    </button>
-                ) : (
-                    <button
-                        className="action-button primary-button"
-                        onClick={(e) => onToggleState(e.target, post._id)}
-                    >
-                        Publish
-                    </button>
+                    <VerticalDotsIcon></VerticalDotsIcon>
+                </button>
+                {isMenuOpen && (
+                    <PopupMenu onClickOutside={() => setIsMenuOpen(false)}>
+                        <button
+                            onClick={() => {
+                                setIsMenuOpen(false);
+                                onToggleState(post._id);
+                            }}
+                            className="popup-menu-option"
+                        >
+                            {post.is_published ? (
+                                <>
+                                    <UnpublishIcon></UnpublishIcon>
+                                    <span>Unpublish</span>
+                                </>
+                            ) : (
+                                <>
+                                    <PublishIcon></PublishIcon>
+                                    <span>Publish</span>
+                                </>
+                            )}
+                        </button>
+                        <Link
+                            className="popup-menu-option"
+                            to={`/post/${post._id}`}
+                        >
+                            <EditIcon></EditIcon>
+                            <span>Edit</span>
+                        </Link>
+                        <button
+                            onClick={() => {
+                                setIsMenuOpen(false);
+                            }}
+                            className="popup-menu-option"
+                        >
+                            <DeleteIcon></DeleteIcon>
+                            <span>Delete</span>
+                        </button>
+                    </PopupMenu>
                 )}
             </div>
         </div>

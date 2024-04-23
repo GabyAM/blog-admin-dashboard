8be0eab4ed355d08af7dc536181271a205dec54b
GabyAM <gdgabyam@gmail.com>
2024-04-23 11:27:39 -0300

    Updates post card skeleton
    it was adjusted because i changed the card layout

--- src/components/PostCardSkeleton.jsx
+++ src/components/PostCardSkeleton.jsx
@@ -4,24 +4,16 @@ import 'react-loading-skeleton/dist/skeleton.css';
 export function PostCardSkeleton() {
     return (
         <div className="post-card">
-            <div className="main-section">
-                <div className="post-thumbnail">
-                    <Skeleton width="100%" height="100%"></Skeleton>
-                </div>
-                <div className="text-section flex-col">
-                    <h2>
-                        <Skeleton></Skeleton>
-                    </h2>
-                    <p>
-                        <Skeleton count={3}></Skeleton>
-                    </p>
-                </div>
+            <div className="post-thumbnail">
+                <Skeleton width="100%" height="100%"></Skeleton>
             </div>
-            <div className="actions flex-row">
-                <Skeleton height="100%" borderRadius="100vh"></Skeleton>
-                <Skeleton height="100%" borderRadius="100vh"></Skeleton>
-
-                <Skeleton height="100%" borderRadius="100vh"></Skeleton>
+            <div className="text-section flex-col">
+                <h2>
+                    <Skeleton></Skeleton>
+                </h2>
+                <p>
+                    <Skeleton count={3}></Skeleton>
+                </p>
             </div>
         </div>
     );

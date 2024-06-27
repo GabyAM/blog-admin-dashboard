3065d60842769d5c57fe36093f639956c9dc8b10
GabyAM <gdgabyam@gmail.com>
2024-06-27 12:06:18 -0300

    styles search results tabs
    The tab list now has an underline element that positions below the selected tab and gets adjusted to the tab content width.

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -1,7 +1,8 @@
-import { useMemo, useState } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 import { AllPosts } from './AllPosts';
 import { Comments } from './Comments';
 import { Users } from './Users';
+import '../styles/searchresults.css';
 import { useSearch } from '../hooks/useSearch';
 
 const tabs = ['Posts', 'Users', 'Comments'];
@@ -17,19 +18,32 @@ export function SearchResults() {
         };
     }, [search]);
 
+    const tabsRef = useRef({});
+    const [underlineStyle, setUnderlineStyle] = useState({});
+    const padding = 20;
+    useEffect(() => {
+        if (tabsRef.current) {
+            setUnderlineStyle({
+                width: tabsRef.current[currentTab].offsetWidth + padding,
+                left: tabsRef.current[currentTab].offsetLeft - padding / 2
+            });
+        }
+    }, [tabsRef, currentTab]);
     return (
         <>
             <ul className="tabs-list flex-row">
                 {tabs.map((tab) => (
                     <li key={tab} className="tab-container">
                         <button
-                            className="tab"
+                            ref={(el) => (tabsRef.current[tab] = el)}
+                            className={`tab ${currentTab === tab ? 'selected' : ''}`}
                             onClick={() => setCurrentTab(tab)}
                         >
                             {tab}
                         </button>
                     </li>
                 ))}
+                <div className="tab-underline" style={underlineStyle}></div>
             </ul>
             {results[currentTab]}
         </>

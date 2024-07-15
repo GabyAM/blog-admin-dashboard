ee0d80f603d67169869fb41a9c52b6d83f9d51ef
GabyAM <gdgabyam@gmail.com>
2024-07-15 13:02:39 -0300

    Implements user card on header
    Now a popup menu appears when clicking on the user card, with a button to let the user log out or visit its profile (will redirect to the profile on the main blog app later)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,16 @@
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import '../styles/header.css';
 import { useAuth } from '../hooks/useAuth';
+import { useState } from 'react';
+import { PopupMenu } from './PopupMenu';
+import { LogoutIcon, ProfileIcon } from './Icons';
 import { SearchBar } from './SearchBar';
 
 export function Header() {
     const navigate = useNavigate();
     const { token: currentUser, removeToken } = useAuth();
+
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
     return (
         <header>
             <button
@@ -17,7 +22,37 @@ export function Header() {
             <div className="main-search-bar-section">
                 <SearchBar></SearchBar>
             </div>
-            <div className="user-card"></div>
+            <div>
+                <button
+                    className="header-user-card"
+                    onClick={() => setIsMenuOpen(true)}
+                >
+                    <div className="image-container">
+                        <img
+                            src={`http://localhost:3000${currentUser.image}`}
+                        ></img>
+                    </div>
+                    <div className="user-info">
+                        <span>{currentUser.name}</span>
+                        <span>{currentUser.email}</span>
+                    </div>
+                </button>
+                {isMenuOpen && (
+                    <PopupMenu onClickOutside={() => setIsMenuOpen(false)}>
+                        <Link className="popup-menu-option">
+                            <ProfileIcon></ProfileIcon>
+                            <span>Profile</span>
+                        </Link>
+                        <button
+                            onClick={removeToken}
+                            className="popup-menu-option"
+                        >
+                            <LogoutIcon></LogoutIcon>
+                            <span>Log out</span>
+                        </button>
+                    </PopupMenu>
+                )}
+            </div>
         </header>
     );
 }
